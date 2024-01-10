539f9fa5fed01ac20d90297607ca8b61ef179557
jwalaeswar <jwalaeswarprasad7014@gmail.com>
2024-01-10 22:08:21 +0530

    added

---
+++ 1-50/1. Two Sum/TwoSum.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for(int i = 0; i < nums.length; i++){
+            if(map.containsKey(target - nums[i])){
+                int j = map.get(target - nums[i]);
+                int[] arr = {j, i};
+                return arr;
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        return nums;
+    }
+}
